2613d4c205ee4d8da40058352c400a05a6116ce9
Benknightdark <moon791017@gmail.com>
2017-08-04 13:26:11 +0800

    231

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BooksdashboardService } from "app/services/booksdashboard.service";
 
 @Component({
   selector: 'app-dashboard',
@@ -7,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private BooksdashboardService:BooksdashboardService) { }
 
   ngOnInit() {
+    this.BooksdashboardService.GetBookData().subscribe(a=>console.log(a))
     $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?', function (data) {
+      console.log(data)
       Highcharts.stockChart('container', {
         rangeSelector: {
           selected: 1
@@ -28,7 +31,58 @@ export class DashboardComponent implements OnInit {
         }]
       });
     });
-
+    Highcharts.chart('pie', {
+      chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+      },
+      title: {
+        text: 'Browser market shares January, 2015 to May, 2015'
+      },
+      tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+      },
+      plotOptions: {
+        pie: {
+          allowPointSelect: true,
+          cursor: 'pointer',
+          dataLabels: {
+            enabled: true,
+            format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+            style: {
+              color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+            }
+          }
+        }
+      },
+      series: [{
+        name: 'Brands',
+        colorByPoint: true,
+        data: [{
+          name: 'Microsoft Internet Explorer',
+          y: 56.33
+        }, {
+          name: 'Chrome',
+          y: 24.03,
+          sliced: true,
+          selected: true
+        }, {
+          name: 'Firefox',
+          y: 10.38
+        }, {
+          name: 'Safari',
+          y: 4.77
+        }, {
+          name: 'Opera',
+          y: 0.91
+        }, {
+          name: 'Proprietary or Undetectable',
+          y: 0.2
+        }]
+      }]
+    });
   }
 
 }
--- src/app/services/booksdashboard.service.ts
+++ src/app/services/booksdashboard.service.ts
@@ -1,8 +1,18 @@
+import { RequestOptions, Http, Headers, Response } from '@angular/http';
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import 'rxjs'
+import { BooksData } from "app/Models/BooksData";
 @Injectable()
 export class BooksdashboardService {
+  JSONoptions = new RequestOptions({ headers: new Headers({ 'Content-Type': 'application/json;charset=utf-8' }) });
+  Host = 'http://localhost:3000/books'
+  constructor(private http: Http) { }
+  GetBookData() {
+    return this.http.get(this.Host)
+    .map(res => res.json())
+    .groupBy(data => data.author)
 
-  constructor() { }
 
+  }
 }
