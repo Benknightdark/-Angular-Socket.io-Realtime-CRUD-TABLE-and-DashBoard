3f7698e3b4ceed8737b821aa12628d4c24eaa0b2
Benknightdark <moon791017@gmail.com>
2017-08-14 11:15:58 +0800

    ok

--- src/app/services/booksdashboard.service.ts
+++ src/app/services/booksdashboard.service.ts
@@ -1,7 +1,7 @@
 import { RequestOptions, Http, Headers, Response } from '@angular/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import 'rxjs'
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/operator/map'
 import { BooksData } from 'app/Models/BooksData';
 
 
@@ -9,19 +9,12 @@ import { BooksData } from 'app/Models/BooksData';
 export class BooksdashboardService {
   JSONoptions = new RequestOptions({ headers: new Headers({ 'Content-Type': 'application/json;charset=utf-8' }) });
   Host = 'http://localhost:3000/books'
-
   constructor(private http: Http) { }
   GetBookData() {
     return this.http.get(this.Host)
       .map(res => {
-        let r = alasql('SELECT author AS name, SUM(age) AS y FROM ? GROUP BY author', [res.json()]);
-        return r
+        return alasql('SELECT author AS name, SUM(age) AS y FROM ? GROUP BY author', [res.json()])
       })
   }
-
-
-
-
-
 }
 
